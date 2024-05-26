7fb81face027be3d2501268241a87fdcfff88e90
SurjeetMandal <surjeetmandal698@gmail.com>
2024-05-26 11:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let textInput = document.getElementById('text');
+let delayInput = document.getElementById('delay');
+let outputDiv = document.getElementById('output');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    let data = textInput.value;
+    let delayTime = delayInput.value;
+
+	outputDiv.innerHTML = ` `;
+    
+    setTimeout(() => {
+        outputDiv.innerHTML = data;
+    }, delayTime * 1000);
+});
+
+
+
